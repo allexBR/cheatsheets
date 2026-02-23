b80122cebab485bc2e7d3308f9cfb077c2c157e4
allexBR <87540794+allexBR@users.noreply.github.com>
2026-02-23 12:55:53 -0300

    Update and rename install-adguardhome.sh to install-adguardhome.sh

--- hands-on/AdGuardHome/install-adguardhome.sh
+++ hands-on/AdGuardHome/install-adguardhome.sh
@@ -5,6 +5,7 @@
 # -----------------------------------------------------------------------------------
 
 # --- Validating privileges and re-executing as root ---
+# Check if the script is already running as root (UID 0)
 if [ "$(id -u)" -ne 0 ]; then
   echo "This script requires root privileges."
   echo "Enter the root password when prompted to continue."
@@ -14,25 +15,27 @@ if [ "$(id -u)" -ne 0 ]; then
   exec su - -c "/bin/bash \"$SCRIPT_PATH\" $*"
 fi
 
+echo "Starting the AdGuard Home installation. Please wait..."
+
+# Initial System repositories update
 apt clean ; apt update ; apt upgrade -y
 
+# Enter to the folder where AdGuard Home will be installed
 cd /usr/local/etc/
 
+# Download AdGuard Home (latest stable release) source code
 wget https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_amd64.tar.gz
 
+# Extract AdGuard Home source code
 tar xzf AdGuardHome_linux_amd64.tar.gz
 
+# Enter the directory extracted from the compressed file
 cd AdGuardHome
 
+# Start AdGuard Home as a System service
 sudo ./AdGuardHome -s install
 
-#------------------------------------------
-# WebGUI access: http://<IP-or-FQDN>:3000
-#------------------------------------------
-
-#------------------------------------------
-# HTTPS WebGUI Config:
-#------------------------------------------
+# HTTPS webGUI config (generate a self-signed certificate)
 tee /usr/local/etc/AdGuardHome/openssl-san.ext <<EOF
 # -----------------------------------------------------------------
 # san_adguard_home.ext (v3-ext)
@@ -50,8 +53,7 @@ C  = CY
 ST = Limassol District
 L  = Limassol
 O  = AdGuard
-OU = AdGuard-Home
-CN = localhost
+CN = adguard.com
 
 [v3_req]
 basicConstraints = CA:FALSE
@@ -65,6 +67,7 @@ IP.1  = 127.0.0.1
 IP.2  = ::1
 EOF
 
+# Create a ECDSA self-signed AdGuard private key and certificate using SAN
 openssl req -x509 -newkey ec \
   -pkeyopt ec_paramgen_curve:secp384r1 \
   -keyout /etc/ssl/private/adguard.key \
@@ -75,12 +78,16 @@ openssl req -x509 -newkey ec \
   -config /usr/local/etc/AdGuardHome/openssl-san.ext \
   -extensions v3_req
 
-chmod 600 /etc/ssl/private/adguard.key
-
-chmod 644 /etc/ssl/certs/adguard.crt
+# Configure necessary permissions
+chmod 600 /etc/ssl/private/adguard.key && chmod 644 /etc/ssl/certs/adguard.crt
 
 chown root:root /etc/ssl/private/adguard.key /etc/ssl/certs/adguard.crt
 
 # nano /usr/local/etc/AdGuardHome/AdGuardHome.yaml
 
 # systemctl restart AdGuardHome
+
+#-------------------------------------------------#
+#  WebGUI first access: http://<IP-or-FQDN>:3000  #
+#-------------------------------------------------#
+
