f530727fd7cc3929575d3bd526b6de290a2daf1e
allexBR <87540794+allexBR@users.noreply.github.com>
2026-02-27 11:25:51 -0300

    Update install-intelowl.sh

--- hands-on/IntelOwl/install-intelowl.sh
+++ hands-on/IntelOwl/install-intelowl.sh
@@ -4,7 +4,7 @@
 # Created by allexBR | https://github.com/allexBR
 # Sources: https://intelowlproject.github.io/
 #          https://github.com/intelowlproject/IntelOwl
-# Last review date: Fri Feb 27 11:01:47 UTC 2026
+# Last review date: Fri Feb 27 11:25:47 UTC 2026
 # -----------------------------------------------------------------------------
 
 # Validating privileges and re-executing as root
@@ -21,10 +21,108 @@ if [ "$(id -u)" -ne 0 ]; then
     exit $?
 fi
 
+echo "Starting IntelOwl installation. Please wait..."
+
+# Initial System repositories update
+apt clean ; apt update ; apt upgrade -y
+
 # Define working directory
 WORK_DIR="/opt"
 cd "$WORK_DIR" || exit 1
 
 echo "[+] Operating in the directory: $WORK_DIR"
 
-#https://github.com/intelowlproject/IntelOwl/releases/
+# Clone the IntelOwl project repository
+git clone https://github.com/intelowlproject/IntelOwl
+
+# Enter in the project directory
+cd IntelOwl/
+
+# Run helper script to verify installed dependencies and configure basic stuff
+./initialize.sh
+
+# HTTPS webGUI config (generate a self-signed certificate / important: test only!)
+tee /opt/IntelOwl/configuration/nginx/openssl-san.conf <<EOF
+# -----------------------------------------------------------#
+# openssl-san.ext (v3-ext)                                   #
+# X.509 extensions for adding SAN to self-signed certificate #
+# -----------------------------------------------------------#
+
+[req]
+distinguished_name = req_distinguished_name
+req_extensions     = v3_req
+x509_extensions    = v3_req
+prompt             = no
+
+[req_distinguished_name]
+C  = US
+ST = CA
+L  = Berkeley
+O  = Trusted-CA
+CN = Root
+
+[v3_req]
+basicConstraints = CA:FALSE
+keyUsage         = critical, digitalSignature, keyEncipherment, keyAgreement
+extendedKeyUsage = serverAuth
+subjectAltName   = @alt_names
+
+[alt_names]
+DNS.1 = localhost
+IP.1  = 127.0.0.1
+IP.2  = ::1
+EOF
+
+# Create a ECDSA self-signed IntelOwl private key and certificate using SAN
+openssl req -x509 -newkey ec \
+  -pkeyopt ec_paramgen_curve:secp384r1 \
+  -keyout /etc/ssl/private/intelowl.key \
+  -out /etc/ssl/certs/intelowl.crt \
+  -sha256 \
+  -days 36500 \
+  -nodes \
+  -config /opt/IntelOwl/configuration/nginx/openssl-san.ext \
+  -extensions v3_req
+
+# Configure necessary permissions
+chmod 600 /etc/ssl/private/intelowl.key && chmod 644 /etc/ssl/certs/intelowl.crt
+
+chown root:root /etc/ssl/private/intelowl.key /etc/ssl/certs/intelowl.crt
+
+# IntelOwl web server config
+tee /opt/IntelOwl/configuration/nginx/https.conf <<EOF
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_certificate /etc/ssl/certs/intelowl.crt;
+    ssl_certificate_key /etc/ssl/private/intelowl.key;
+EOF
+
+cat /opt/IntelOwl/configuration/nginx/https.conf
+
+# Check IntelOwl web server config changes
+nginx -t
+
+# Restart IntelOwl web server
+systemctl reload nginx
+
+# Start the app
+./start prod up
+
+#---------------------------------
+# Start the app in https mode
+#./start prod up --https
+#---------------------------------
+
+# Now the application is running on http://localhost:80
+
+# Create a super user
+# docker exec -ti intelowl_uwsgi python3 manage.py createsuperuser
+
+
+# Now you can login with the created user from http://localhost:80/login
+
+# Have fun!
+
+#-----------------------------------------------
+# apt install -y python3-pip python-is-python3
+#
+# python -m pip install --upgrade pywin32
