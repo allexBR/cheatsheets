5f9ccffdf6d4eac7aefdd0de0725622f5b2bb5d3
allexBR <87540794+allexBR@users.noreply.github.com>
2026-04-16 14:12:38 -0300

    Update ntopng-ssl-cert.sh

--- hands-on/Suricata/ntopng-ssl-cert.sh
+++ hands-on/Suricata/ntopng-ssl-cert.sh
@@ -3,7 +3,7 @@
 # Generating self-signed SSL/TLS certificates for ntopng
 # IMPORTANT: Do not use this in a prod environment, only for testing!
 # Created by allexBR | https://github.com/allexBR
-# Last review date: Thu Apr 16 10:51:08 UTC 2026
+# Last review date: Thu Apr 16 14:12:38 UTC 2026
 # -----------------------------------------------------------------------------------
 
 # Validating privileges and re-executing as root
